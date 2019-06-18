d7cf18d270f318b51132d465f008d7dbd6818301
Nina Kazzim <kazzim@icloud.com>
2019-06-18 23:43:09 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_numbers = []
-  array.each do |array|
-   new_numbers << array ** 2
+  array.each do |element|
+   new_numbers << element ** 2
  end
  new_numbers
 end
